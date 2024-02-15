fe4a54dc39d9de0ce0fd514a681cb52af1539e0f
Sobhit Singhal <128816933+sobhitsinghal@users.noreply.github.com>
2024-02-15 23:06:26 +0530

    Create String.java

---
+++ src/basicjava/String.java
@@ -0,0 +1,13 @@
+public class StringDemo {
+
+public static void main(String args[]) {
+
+String palindrome = "Dot saw I was Tod";
+
+int len = palindrome.length();
+
+System.out.println( "String Length is : " + len );
+
+}
+
+}
