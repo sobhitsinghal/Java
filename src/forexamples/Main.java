f843b7b2b2bf0a887f92fd2e4c2c9494a225ad15
Sobhit Singhal <128816933+sobhitsinghal@users.noreply.github.com>
2024-02-17 23:46:38 +0530

    Create Main.java

---
+++ src/forexamples/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+public static void main(String[] args) {
+Scanner scn = new Scanner(System.in);
+int marks = scn.nextInt();
+
+if(marks>=35) {
+System.out.println("pass");
+}else{
+System.out.println("fail");
+}
+}
+}
