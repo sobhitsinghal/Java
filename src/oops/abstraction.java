ecf9150b58b631ea3d3e26bfb6b3623d22ad1d67
Sobhit Singhal <128816933+sobhitsinghal@users.noreply.github.com>
2024-02-27 22:31:23 +0530

    Create abstraction.java

---
+++ src/oops/abstraction.java
@@ -0,0 +1,38 @@
+//implementation of abstraction through abstract class 
+abstract class Animal { 
+	abstract void walk(); 
+	void eat() 
+	{ 
+		System.out.println("The animal is eating."); 
+	} 
+	Animal() 
+	{ 
+		System.out.println( 
+			"An Animal is going to be created."); 
+	} 
+} 
+
+class Cow extends Animal { 
+	Cow() { System.out.println("You have created a Cow"); } 
+	void walk() { System.out.println("Cow is walking."); } 
+} 
+
+class Goat extends Animal { 
+	Goat() 
+	{ 
+		System.out.println("You have created a Goat"); 
+	} 
+	void walk() { System.out.println("Goat is walking."); } 
+} 
+
+public class OOPS { 
+	public static void main(String args[]) 
+	{ 
+		Cow cow = new Cow(); 
+		cow.walk(); 
+		cow.eat(); 
+		Goat goat = new Goat(); 
+		goat.walk(); 
+		goat.eat(); 
+	} 
+}
